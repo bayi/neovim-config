b96793ec01e00c0237fdcf54aee083f28886a308
Bayi <bayi@bayi.hu>
2023-07-24 17:55:31 +0200

    Document keybindings

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -22,25 +22,25 @@ return {
         },
         init = function()
             vim.keymap.set('n', '<leader>fo', require('telescope.builtin').oldfiles,
-                { desc = '[?] Find recently opened files' })
+                { desc = 'Find recently opened files' })
             vim.keymap.set('n', '<leader>b<space>', require('telescope.builtin').buffers,
-                { desc = '[ ] Find existing buffers' })
+                { desc = 'Find existing buffers' })
             vim.keymap.set('n', '<leader>b/', function()
                 -- You can pass additional configuration to telescope to change theme, layout, etc.
                 require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
                     winblend = 10,
                     previewer = false,
                 })
-            end, { desc = '[/] Fuzzily search in current buffer' })
+            end, { desc = 'Fuzzily search in current buffer' })
 
-            vim.keymap.set('n', '<leader>fg', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
-            vim.keymap.set('n', '<C-p>', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
-            vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
+            vim.keymap.set('n', '<leader>fg', require('telescope.builtin').git_files, { desc = 'Search Git Files' })
+            vim.keymap.set('n', '<C-p>', require('telescope.builtin').find_files, { desc = 'Search Files' })
+            vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = 'Search Help' })
             vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string,
-                { desc = '[S]earch current [W]ord' })
-            vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
+                { desc = 'Search current Word' })
+            vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = 'Search by Grep' })
             vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics,
-                { desc = '[S]earch [D]iagnostics' })
+                { desc = 'Search Diagnostics' })
             -- Enable telescope fzf native, if installed
             pcall(require('telescope').load_extension, 'fzf')
         end,
